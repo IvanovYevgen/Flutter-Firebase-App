2cf421a9249014624626db1c26b17ad7f4986e81
Yevhen Ivanov <ivanovev77@gmail.com>
2020-11-11 23:30:42 +0200

    implementation of validation method

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+
+  void validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      print('Form is valid');
+    } else {
+      print('Form is invalid');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter login demo'),
+        centerTitle: true,
       ),
       body: Container(
-        child: Text('Hello world!'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(labelText: 'Email'),
+                    validator: (value) =>
+                        value.isEmpty ? 'Email can\'t be empty' : null),
+                TextFormField(
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) =>
+                        value.isEmpty ? 'Password can\'t be empty' : null),
+                RaisedButton(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: validateAndSave),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
