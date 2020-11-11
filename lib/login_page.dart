607552c87a8f6e12d017e0cab8bf7723d90396b8
Yevhen Ivanov <ivanovev77@gmail.com>
2020-11-11 22:09:43 +0200

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter login demo'),
+      ),
+      body: Container(
+        child: Text('Hello world!'),
+      ),
+    );
+  }
+}
